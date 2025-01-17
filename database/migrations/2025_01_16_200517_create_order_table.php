2a44cb9cc2bd7d8b31cd841869749fc8c4e9728c
zh4r11 <azharimurii@gmail.com>
2025-01-17 15:19:51 +0700

    first commit feature order

---
+++ database/migrations/2025_01_16_200517_create_order_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('id_pelanggan')->constrained('pelanggan');
+            $table->string('telepon');
+            $table->string('no_order');
+            $table->enum('status', ['new', 'processed', 'shiped', 'delivered', 'completed', 'canceled']);
+            $table->string('provinsi');
+            $table->string('kota');
+            $table->string('kecamatan');
+            $table->string('kelurahan');
+            $table->string('kode_pos');
+            $table->text('alamat');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
